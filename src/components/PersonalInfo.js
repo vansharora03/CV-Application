b7161adea709ae82c98ce29607d74884765217fb
vansharora03 <vansh1993@icloud.com>
2023-01-07 15:36:51 -0500

    Submitted fields now update text based on input
    -Only affects PersonalInfo for now

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -14,42 +14,54 @@ export default class PersonalInfo extends React.Component {
                     url: ''
                 }
             }
+            this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState(prevState => ({
+            ...prevState,
+            user: {
+                ...prevState.user,
+                [e.target.id]: e.target.value
+            }
+        }))
     }
 
 
 
     render() {
+        const { firstName, lastName, email, phone, url } = this.state.user;
         const editable = ( <div className="header">
         <div className="name">
             <label htmlFor="firstName">First Name</label>
-            <input id="firstName"></input>
+            <input onChange={this.handleChange} id="firstName" value={firstName}></input>
 
             <label htmlFor="lastName">Last Name</label>
-            <input id="lastName"></input>
+            <input onChange={this.handleChange} id="lastName" value={lastName}></input>
         </div>
         <div className="miscInfo">
             <label htmlFor="email">Email</label>
-            <input id="email" type="email"></input>
+            <input onChange={this.handleChange} id="email" type="email" value={email}></input>
 
             <label htmlFor="phone">Phone</label>
-            <input id="phone" type="tel"></input>
+            <input onChange={this.handleChange} id="phone" type="tel" value={phone}></input>
 
             <label htmlFor="url">Website</label>
-            <input id="url" type="url"></input>
+            <input onChange={this.handleChange} id="url" value={url}></input>
         </div>
     </div>)
         const notEditable = (<div className="header">
         <div className="name">
-            <h1 className="firstNameDisplay">First Name</h1>
+            <h1 className="firstNameDisplay">{firstName}</h1>
 
-            <h1 htmlFor="lastNameDisplay">Last Name</h1>
+            <h1 htmlFor="lastNameDisplay">{lastName}</h1>
         </div>
         <div className="miscInfo">
-            <p className="emailDisplay">Email</p>
+            <p className="emailDisplay">{email}</p>
 
-            <p className="phoneDisplay">Phone</p>
+            <p className="phoneDisplay">{phone}</p>
 
-            <p className="urlDisplay">Website</p>
+            <p className="urlDisplay">{url}</p>
         </div>
     </div>)
 
