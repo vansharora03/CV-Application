0a0a4aef552490afddcb39b11d6427479405ad6a
vansharora03 <vansh1993@icloud.com>
2023-01-07 16:37:10 -0500

    EducationView now displays all education entries
    -Takes data from parent Education's state

--- src/components/EducationView.js
+++ src/components/EducationView.js
@@ -1,7 +1,24 @@
 import React from "react";
+import '../styles/EducationView.css';
 
 export default class EducationView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
     render() {
-        return (<div></div>)
+        const {educationList} = this.props;
+        const educationEntries = educationList.map((entry) => {
+            return (
+                <div key={entry.id}className="education-entry">
+                    <h2 key={`schoolNameDisplay-${entry.id}`} className="schoolNameDisplay">{entry.school}</h2>
+                    <h3 key={`areaOfStudyDisplay-${entry.id}`} className="areaOfStudyDisplay">{entry.areaOfStudy}</h3>
+                    <h3 key={`graduationDateDisplay-${entry.id}`} className="graduationDateDisplay">{entry.graduationDate}</h3>
+                </div>
+
+            )
+        })
+        return (<div className="EducationView">{educationEntries}</div>)
     }
 }
