733b80c155914195c31491e24698dac95e989e79
vansharora03 <vansh1993@icloud.com>
2023-01-07 19:15:49 -0500

    Created WorkView.js and imported it in Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,5 @@
 import React from "react";
+import WorkView from "./WorkView";
 
 export default class Work extends React.Component {
     render() {
---
+++ src/components/WorkView.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class WorkView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (<div className="WorkView"></div>)
+        }
+}
