ed65b6bba436adf3de859e85575686b93f82802e
vansharora03 <vansh1993@icloud.com>
2023-01-07 23:05:37 -0500

    Mapped data to elements in WorkView to display

--- src/components/WorkView.js
+++ src/components/WorkView.js
@@ -8,6 +8,18 @@ export default class WorkView extends React.Component {
     }
 
     render() {
-        return (<div className="WorkView"></div>)
+        const {workList} = this.props;
+        const workEntries = workList.map(entry => {
+            return(
+                <div className="workEntry" key={entry.id}>
+                    <h2 key={`companyDisplay-${entry.id}`} className="companyDisplay">{entry.company}</h2>
+                    <h3 key={`positionDisplay-${entry.id}`} className="positionDisplay">{entry.position}</h3>
+                    <h3 key={`descriptionDisplay-${entry.id}`} className="descriptionDisplay">{entry.description}</h3>
+                    <p key={`dateDisplay-${entry.id}`} className="dateDisplay">{entry.startDate.substring(0,4)} - {entry.endDate.substring(0, 4)}</p>
+                    
+                </div>
+            )
+        })
+        return (<div className="WorkView">{workEntries}</div>)
         }
 }
